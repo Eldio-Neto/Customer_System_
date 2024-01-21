9cc13703b206c461d4703fcfe5695af08313c4c9
Eldio Neto <eldioneto2021@gmail.com>
2024-01-21 15:43:12 -0300

    Add fillable and relationship in Pedido_image model

--- back-end/app/Models/Pedido_image.php
+++ back-end/app/Models/Pedido_image.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido_image extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['pedido_id', 'imagem', 'capa'];
+
+    public function pedido_image()
+    {
+        return $this->hasOne(Pedido_image::class);
+    }
 }
