66d503829061ff5c022ddc115e3926a8097984a4
Eldio Neto <eldioneto2021@gmail.com>
2024-01-21 15:43:48 -0300

    Update header title in Header component

--- front-end/src/components/Header.tsx
+++ front-end/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header className="flex justify-between items-center p-5 bg-blue-500 text-white">
-      <h1>customer_system</h1>
+      <h1 className="text-2xl font-bold">Customer System {'</>'}</h1>
       <nav>
         <Link to="/clientes" className="mr-4">Clientes</Link>
         <Link to="/pedidos">Pedidos</Link>
